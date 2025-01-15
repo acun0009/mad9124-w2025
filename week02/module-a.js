7350afdcdbe3d70bf3594ea4c6800ce31fa12699
Atlas <acun0009@algonquinlive.com>
2025-01-15 14:22:59 -0500

    reorganized files

--- week02/module-a.js
+++
@@ -1,7 +0,0 @@
-const { someOtherVar, sayHello } = require('./module-b')
-const someVar = "A";
-
-sayHello('Atlas')
-
-console.log(someVar)
-console.log(someOtherVar);
--- week02/module-b.js
+++
@@ -1,8 +0,0 @@
-const someOtherVar = "B";
-const sayHello = (name) => {
-    console.log(`Hello, ${name}`);
-}
-module.exports = {
-    someOtherVar,
-    sayHello
-};
