cf9297750c042e7a0a9d3cf37602a3fd1b33422e
Atlas <acun0009@algonquinlive.com>
2025-01-15 14:19:44 -0500

    finished exercise 1

--- week01/exercise/helloworld.js
+++ week01/exercise/helloworld.js
@@ -1,5 +1,40 @@
+const students = require('./students.json');
+
+//function that takes a message to log
 function say(message) {
     console.log(message)
 }
 
-say('Hello, world!');
+/* iterates over students array and logs their first and last name
+with Array.forEach
+*/
+students.forEach(({ firstName, lastName }) => say(`Hello ${firstName} ${lastName}`));
+
+// just to separate code
+console.log("*************")
+
+/* loops through students array with Array.reduce
+if the last name starts with 'D', we add +1 to the
+accumulator
+*/
+const count = students.reduce((accumulator, {lastName}) => {
+    if(lastName.startsWith('D')) {
+        accumulator += 1;
+    }
+    return accumulator;
+}, 0)
+
+console.log(`Count of last names starting with D is ${count}`)
+
+// just to separate code
+console.log("*************")
+
+/* uses Array.map to loop through the students array
+and returns the first name + email suffix
+*/
+const emails = students.map(({ firstName }) => {
+    const emailSuffix = "@algonquincollege.com";
+    return (firstName.toLowerCase() + emailSuffix);
+});
+
+console.log(emails);
