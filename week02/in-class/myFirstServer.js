8804b91a9ed447f3b2b5c9f47b410aa447f52028
Atlas <acun0009@algonquinlive.com>
2025-01-24 16:42:04 -0500

    modified and deleted some files

--- week02/in-class/myFirstServer.js
+++ week02/in-class/myFirstServer.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const http = require('http');
+const students = require('../../week01/exercise/students.json');
 const server = http.createServer(( request, response) => {
     if (request.url === '/feed') {
         response.write('This is your news feed');
     } else if (request.url === '/username') {
         response.write('Atlas');
+    } else if (request.url === '/api') {
+        response.setHeader('Content-Type', 'application/json')
+        response.write(JSON.stringify(students));
     } else {
-        response.write('Hello World from node.js');
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json')
+        response.write(JSON.stringify({error: 'url not ofund'}));
     }
     response.end();
 })
